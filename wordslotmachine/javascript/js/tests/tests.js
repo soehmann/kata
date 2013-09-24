4e3fd1c9d189456d78a55ae0b8329440c99c2842
XSilent <michael.neuhaus@gmx.de>
2013-09-24 21:19:37 +0200

    Skeleton for kata WordSlotmachine - JavaScript version - including QUnit and jQunit - ready to go!

---
+++ wordslotmachine/javascript/js/tests/tests.js
@@ -0,0 +1,15 @@
+/**
+ * WordSlotmachine - Testsuite ( using QUnit )
+ */
+
+test( "test the test (Examples)", function() {
+
+	ok( true, "true succeeds" );
+	equal( 0, 0, "Zero; equal succeeds" );
+	equal( '', 0, "Zero; equal succeeds" );
+
+	strictEqual( 0, 0, "Comparision with strict" );
+
+	deepEqual( 0, 0, 'deepequel' );
+});
+
